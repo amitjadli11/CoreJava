77bf92c472ee12039774417b6d494c2843658760
Amit <SANJAY@SANJAY-PC.connectify>
2016-10-10 23:32:40 +0530

    here differnce between two number is commited

---
+++ CoreJava/src/com/Core/Subtraction.java
@@ -0,0 +1,10 @@
+package com.Core;
+
+public class Subtraction {
+	public static void main(String []k){
+		int a=7;
+		int b=4;
+		System.out.println("the differnce between two number is "+(a-b));
+	}
+
+}
