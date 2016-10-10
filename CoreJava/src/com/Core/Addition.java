bcbcb2d6aa408c3de9402c7f07ac1321b1fc4346
Amit <SANJAY@SANJAY-PC.connectify>
2016-10-10 22:00:36 +0530

    This is my first commit...

---
+++ CoreJava/src/com/Core/Addition.java
@@ -0,0 +1,12 @@
+package com.Core;
+
+public class Addition {
+
+	
+	public static void main(String[] k){
+		int a=4;
+		int b=3;
+		int c=a+b;
+		System.out.println("the sum of two number is="+c);
+	}
+}
